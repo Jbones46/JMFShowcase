255e506b2ffaf708b6d4cdc3667e9dc135ee83b8
Justin Ferre <Jman4647@gmail.com>
2015-10-16 20:28:45 -0400

    added email and password user sign up

--- JMFShowcase/Constants.swift
+++ JMFShowcase/Constants.swift
@@ -11,8 +11,15 @@ import UIKit
 
 let SHADOW_COLOR: CGFloat = 157.0/255.0
 
+//keys
+
 let KEY_UID = "uid"
 
 //Segues
 
 let SEGUE_LOGGED_IN = "loggedIn"
+
+//status codes
+
+let STATUS_ACCOUNT_NONEXIST = -8
+
--- JMFShowcase/ViewController.swift
+++ JMFShowcase/ViewController.swift
@@ -68,7 +68,36 @@ class ViewController: UIViewController {
     
     @IBAction func attemptLogin(sender: UIButton!) {
         if let email = emailField.text where email != "", let pwd = passwordField.text where pwd != "" {
-            
+            DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { error, authData in
+                
+                if error != nil {
+                    print(error.code)
+                    
+                    if error.code == STATUS_ACCOUNT_NONEXIST {
+                        
+                        DataService.ds.REF_BASE.createUser(email, password: pwd, withValueCompletionBlock: { error, result in
+                            
+                            if error != nil {
+                                
+                                self.showErrorAlert("Could not create account", msg: "Problem creating account")
+                                
+                            } else {
+                                NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
+                                
+                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+                                self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                            }
+                            
+                        })
+                    }else {
+                        self.showErrorAlert("Could not log in", msg: "Please try again")
+                    }
+                    
+                }else {
+                    self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                }
+                
+            })
             
             
         } else {
